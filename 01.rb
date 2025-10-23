80cfefa86da523788298422566294ba10c9d7c04
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-10-23 16:02:51 +0600

    Create 01.rb

---
+++ 01.rb
@@ -0,0 +1,14 @@
+#Building a Mad Libs Game 
+
+puts "Enter a color: "
+color=gets.chomp()
+
+puts "Enter a plural noun: "
+plural_noun=gets.chomp()
+
+puts "Enter a celebrity: "
+celebrity=gets.chomp()
+
+puts ("\nRoses are "+color)
+puts (plural_noun+" are blue.")
+puts ("I love "+celebrity)
