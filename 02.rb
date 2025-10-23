ed1c3d8c5a9b51d6d6e331be23d20f00a12f6d3d
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-10-23 16:08:10 +0600

    Create 02.rb

---
+++ 02.rb
@@ -0,0 +1,10 @@
+#Building a Calculator (for integer)
+
+puts "Enter 1st Number: "
+num1=gets.chomp()
+
+puts "Enter 2nd Number: "
+num2=gets.chomp()
+
+puts "So, total: "
+puts (num1.to_i+num2.to_i)
